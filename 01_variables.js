59940b1e34c24a2652cb3e5acee1b7eb1828ac42
Abhishek <singhchauhanabhishek816@gmail.com>
2023-09-20 12:09:35 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const my_name = "Abhishek";
+let age = 28;
+var profession = "Software Engineer";
+livingState = "Uttar Pradesh"
+
+console.log(typeof(livingState));
+
+console.table([my_name,age,profession,livingState])
