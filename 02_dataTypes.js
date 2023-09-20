a1cf654c632474cd87e3ee0e2ab6165a1c8e767f
Abhishek <singhchauhanabhishek816@gmail.com>
2023-09-20 12:52:45 +0530

    Add Data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,20 @@
+let age = 20;
+let name = "Abhishek"
+let bigNumber = BigInt("123456789012345678901234567890");
+let isAppeared = true;
+let my_name = {firstname : "Abhishek", lastname : "Chauhan"}
+
+
+console.log(typeof age);  //It gives an Integer Data type
+
+console.log(typeof name);// It gives a String Datatype
+
+console.log(typeof bigNumber); //It give a bigint Datatype
+
+console.log(typeof isAppeared); //It gives a boolean Datatype
+
+console.log(typeof my_name); //It gives an Object which are not primitive data type
+
+//Also there are more data types which are null undefined and symbols ..
+
+// Explore more on Internet
